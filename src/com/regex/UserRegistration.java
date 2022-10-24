3d9b52bd0854f4d52bcafab84f82b9f80dde9b5c
vaidya0303 <vaidya0304@gmail.com>
2022-10-24 13:07:38 +0530

    added method to validate mobile no

--- src/com/regex/UserRegistration.java
+++ src/com/regex/UserRegistration.java
@@ -1,13 +1,15 @@
 package com.regex;
-
-
 /**
  * UC1 :- As a User need to enter a valid First Name
  *        - First name starts with Cap and has minimum 3 characters
- *
  * UC2 :- As a User need to enter a valid Last Name
  *        - Last name starts with Cap and has minimum 3 characters
- * 
+ * UC3 :- As a User need to enter  a valid email
+ *        - E.g. abc.xyz@bl.co.in
+ *        - Email has 3 mandatory parts (abc, bl& co) and 2 optional (xyz & in) with precise @ and . positions
+ * UC4 :- As a User need to follow pre-defined Mobile Format
+ *        - E.g. 91 9919819801
+ *        - Country code follow by space and 10 digit number
  */
 
 /**
@@ -19,7 +21,6 @@ package com.regex;
 /**
  * import scanner class ,matcher,pattern class
  */
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -27,15 +28,12 @@ import java.util.regex.Pattern;
 /**
  * create a class name as UserRegistration
  */
-
 public class UserRegistration {
-
     /**
      * create a object for scanner class
      * Scanner class for user input
      */
     Scanner sc=new Scanner(System.in);
-
     /**
      * create a method name as validateUsername
      * method to check username Valid or Invalid
@@ -43,37 +41,29 @@ public class UserRegistration {
     public void validateUserName() {
 
         System.out.println("Enter UserName");
-
         String userName=sc.next();
-
         /**
          * regex pattern for username
          */
         String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
-
         /**
          * First, the pattern is created using the Pattern.compile() method
          * The first parameter indicates which pattern is being searched for
          */
-
         Pattern p = Pattern.compile(regex);
-
         /**
          * The matcher() method is used to search for the pattern in a string.
          * It returns a Matcher object which contains information about the search that was performed.
          */
         Matcher matcher = p.matcher(userName);
-
         /**
          * boolean data type is used for return op is true or false
          */
         boolean result = matcher.matches();
-
         /**
          *  if else conditional statment is used
          *  if pattern match then print  valid username if not matched print invalid username
          */
-
         if(result){
             System.out.println("Valid username");
         }
@@ -81,7 +71,6 @@ public class UserRegistration {
             System.out.println("Invalid username");
         }
     }
-
     /**
      * create a method name as validateLastname
      * method to check LastName Valid or Invalid
@@ -89,32 +78,25 @@ public class UserRegistration {
     public void validateLastName() {
 
         System.out.println("Enter LastName");
-
         String lastName=sc.next();
-
         /**
          *  regex pattern for Lastname
          */
         String regex = "^[A-Z]{1}[a-zA-z0-9]{2,}$";
-
         /**
          * First, the pattern is created using the Pattern.compile() method
          * The first parameter indicates which pattern is being searched for
          */
         Pattern p = Pattern.compile(regex);
-
         /**
          * The matcher() method is used to search for the pattern in a string.
          * It returns a Matcher object which contains information about the search that was performed.
          */
-
         Matcher matcher = p.matcher(lastName);
-
         /**
          * boolean data type is used for return op is true or false
          */
         boolean result = matcher.matches();
-
         /**
          * if mather matches then print valid username other print invalid username
          */
@@ -126,6 +108,60 @@ public class UserRegistration {
         }
     }
 
+    /**
+     * create a method name as validateEmail
+     *  method to check Email Valid or Invalid
+     */
+    public void validateEmail() {
+        System.out.println("Enter Email");
+        String email=sc.next();
+
+        /**
+         * regex pattern for email
+         * 1)must contain character before @
+         * 2)must contain @ symbol after char
+         * 3)must contain char after @
+         * 4)must contain "."  symbol before com or in
+         */
+        String regex = "^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?";
+        Pattern p = Pattern.compile(regex);
+        Matcher matcher = p.matcher(email);
+        boolean result = matcher.matches();
+
+        if(result){
+            System.out.println("Valid Email Address");
+        }
+        else {
+            System.out.println("Invalid Email Address");
+        }
+    }
+
+    /**
+     * create a method name as validMobileNo
+     * method to check Phoneno Valid or Invalid
+     */
+    public void validMobileNo() {
+        System.out.println("Enter MobileNo with country code");
+        String mobileno=sc.next();
+        /**
+         *  regex pattern for email
+         */
+        String regex =  "^[1-9]{2}\\s[0-9]{10}$";
+
+        Pattern p = Pattern.compile(regex);
+        Matcher matcher = p.matcher(mobileno);
+
+        boolean result = matcher.matches();
+        /**
+         * if match print valid MobileNo other print invalid mobileno
+         */
+        if(result){
+            System.out.println("Valid MobileNo");
+        }
+        else {
+            System.out.println("Invalid MobileNo");
+        }
+    }
 
     /**
      * create a main method
@@ -145,20 +181,18 @@ public class UserRegistration {
          * while loop is used
          */
         while(true) {
-
             /**
              * userinput choice for checking Multiple validation
              */
             System.out.println("Enter choice.............\n "
                     +"1)UserName\n"   //uc1
-                    +"2)LastName\n");
+                    +"2)LastName\n"  //uc2
+                    +"3)Email\n"      //uc3
+                    +"4)PhoneNo\n");  //uc4
 
             int choice=sc.nextInt();
-
-
             /**
              * using switch case for choosing option and print output calling wise.
-             *
              */
             switch(choice) {
 
@@ -168,18 +202,29 @@ public class UserRegistration {
                 case 1:
                     registration.validateUserName();
                     break;
-
                 /**
                  * for uc2 calling validateLastname method in this case
                  */
                 case 2:
                     registration.validateLastName();
                     break;
+                /**
+                 * for uc3 calling validateEmail method in this case
+                 */
+                case 3:
+                    registration.validateEmail();
+                    break;
+                /**
+                 * for uc4 calling validMobileNo method in this case
+                 */
+                case 4:
+                    registration.validMobileNo();
+                    break;
 
                 default:
                     System.out.println("Invalid choice");
                     System.exit(0);
-             }
+            }
         }
     }
 }
